3cea669b3eeec366aa4193eba9fd1bd34ac05a84
kaminiupadhayay <kaminiupadhayay@gmail.com>
2019-02-21 22:07:27 +0530

    ok

--- application/modules/editor/views/editor-view.php
+++ application/modules/editor/views/editor-view.php
@@ -7,10 +7,8 @@ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 <head>
 	<meta charset="utf-8">
 	<title>CKEditor Sample</title>
-	<script src="../ckeditor.js"></script>
-	<script src="<?php echo base_url();?>ckeditor/js/sample.js"></script>
-	<link rel="stylesheet" href="<?php echo base_url();?>ckeditor/css/samples.css">
-	<link rel="stylesheet" href="<?php echo base_url();?>ckeditor/toolbarconfigurator/lib/codemirror/neo.css">
+
+	<script src="https://cdn.ckeditor.com/4.11.2/standard/ckeditor.js"></script>
 </head>
 <body id="main">
 
@@ -27,21 +25,13 @@ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 	<div class="adjoined-bottom">
 		<div class="grid-container">
 			<div class="grid-width-100">
-				<div id="editor">
-					<h1>Hello world!</h1>
-					<p>I'm an instance of <a href="https://ckeditor.com">CKEditor</a>.</p>
-				</div>
+				
+				<textarea name="editor1" style="height: 400px; width: auto;"></textarea>
+
 			</div>
 		</div>
 	</div>
 
-	<div class="grid-container">
-		<div class="content grid-width-100">
-			
-
-			
-		</div>
-	</div>
 </main>
 
 <footer class="footer-a grid-container">
@@ -49,9 +39,8 @@ For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 		
 	</div>
 </footer>
-<script>
-	initSample();
-</script>
-
+ <script>
+    CKEDITOR.replace('editor1');
+  </script>
 </body>
 </html>
